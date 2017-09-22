d22ffa2f9e0c7e528efb2851ca7d73075e3c60d9
Dylan Goss <Dylangoss21@yahoo.com>
2017-09-22 14:40:06 -0700

    something

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,7 +4,9 @@ export default class Dashboard extends Component {
     render() {
         return (
             <div className='App'>
-                This is the Dashboard
+                <div className='header'>
+                
+                </div>
                 <a href='http://localhost:3030/auth/logout'><div className='logoutButton'>Logout</div></a>
             </div>
         )
