3d86a691e0e4d3150db801048b145de13cef158a
Dylan Goss <Dylangoss21@yahoo.com>
2017-09-22 09:57:20 -0700

    created backend server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
+
 const express = require ('express'),
       bodyParser = require ('body-parser'),
       massive = require ('massive'),
       session = require  ('express-session'),
       passport = require ('passport'),
       Auth0Strategy = require ('passport-auth0');
-      require('dotenv').config();
 
 
 const app = express();
@@ -19,6 +20,41 @@ app.use(passport.session());
 massive(process.env.CONNECTION_STRING)
 .then( db =>{
     app.set('db', db);
+})
+
+passport.use( new Auth0Strategy({
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK
+}))
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+})
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+})
+
+app.get('/auth', passport.authenticate('auth0'));
+
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/#/dashboard',
+    failureRedirect: '/auth'
+}))
+
+app.get('/auth/user', (req, res) => {
+    if(!req.user) {
+        return res.status(404).send('User not found');
+    } else {
+        return res.status(200).send(req.user);
+    }
+})
+
+app.get('/auth/logout', (req, res) => {
+    req.logOut();
+    res.redirect(302, 'http://localhost:3000/')
 })
 
 
