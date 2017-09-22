6fb9d095985c47954b22b6f29b7910fd5c1ec69e
Dylan Goss <Dylangoss21@yahoo.com>
2017-09-22 15:36:57 -0700

    css for dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import './Dashboard.css';
+import homeLogo from '../../home.png';
+import searchLogo from '../../search.png';
 
 export default class Dashboard extends Component {
     render() {
         return (
-            <div className='App'>
+            <div className='DashboardApp'>
                 <div className='header'>
-                
+                    <div className='heloText'>
+                        <strong>Helo</strong>
+                        <a href='http://localhost:3000/#/dashboard'>
+                        <img className='homeLogo' src={homeLogo}/>
+                        </a>
+                        <img className='search' src={searchLogo}/>
+
+                        <div className='dashboardHeaderText'>
+                        </div>
+                            <strong>Dashboard</strong>
+                            <a href='http://localhost:3030/auth/logout'>
+                                <div className='logoutButton'>
+                                    Logout
+                                </div>
+                            </a>
+                    </div>
                 </div>
-                <a href='http://localhost:3030/auth/logout'><div className='logoutButton'>Logout</div></a>
             </div>
         )
     }
